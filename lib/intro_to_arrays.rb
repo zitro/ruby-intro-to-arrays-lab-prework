66f99c52151eb49cd487c65fa9be6720c4347b26
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-13 17:38:32 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,24 +1,31 @@
 
-
+def instantiate_new_array
   my_new_array = []
+return my_new_array
+end
 
+def array_with_two_elements
   my_two_array = [1, 2]
+  return my_two_array
+end
 
-
-
-
+def indexing
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift[0]
-  taylor_swift[2]
-  taylor_swift.last
+  first_element = taylor_swift[0]
+  third_element = taylor_swift[2]
+  last_element = taylor_swift[-1]
+end
 
 
-
-
+def rubArrReturn
   south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   first_country = south_east_asia.first
   last_country = south_east_asia.last
 
+end
 
+def arrinfo
   programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
   length = programming_languages.length
+
+end
