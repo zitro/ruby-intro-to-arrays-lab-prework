5dbed133aed7da6fc76a4f10fd624a32eac7eb8c
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-13 17:33:11 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,14 +11,20 @@ def indexing
   taylor_swift[0]
   taylor_swift[2]
   taylor_swift.last
-  
-  
+
+
 end
 
 
 def rubArrReturn
   south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   first_country = south_east_asia.first
-  
+  last_country = south_east_asia.last
+
+end
+
+def arrinfo
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  length = programming_languages.length
   
 end
