9dfa1214c1c73ea9cf0640b4bb5745bb45ac2b69
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-13 17:30:51 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+
+def instantiate_new_array
+  my_new_array = []
+
+  my_two_array = [1, 2]
+end
+
+
+def indexing
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[0]
+  taylor_swift[2]
+  taylor_swift.last
+  
+  
+end
+
+
+def rubArrReturn
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country = south_east_asia.first
+  
+  
+end
