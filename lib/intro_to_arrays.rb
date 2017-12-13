6fafca567c3f105c3e856ea68898933e030a5386
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-13 17:44:18 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,19 +4,17 @@ def instantiate_new_array
     return my_new_array
 end
 
-
-
-
 def array_with_two_elements
   my_two_array = [1, 2]
   return my_two_array
 end
 
-def indexing
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  first_element = taylor_swift[0]
-  third_element = taylor_swift[2]
-  last_element = taylor_swift[-1]
+def first_element
+  taylor_swift[0]
+  #taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  #first_element = taylor_swift[0]
+  #third_element = taylor_swift[2]
+  #last_element = taylor_swift[-1]
 end
 
 
